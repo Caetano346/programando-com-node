7644e3010ff5c033a8142d87ff17bb5cacbd3131
João Caetano <118355857+Caetano346@users.noreply.github.com>
2023-12-07 09:50:34 -0400

    Update database-memory.js

--- database-memory.js
+++ database-memory.js
@@ -1,23 +1,19 @@
 import { randomUUID } from "crypto"
 
-
 export class DatabaseMemory{
-    #livros = new Map()
+#livros = new Map()
 
 list(){
-    return this.#livros.values()
+    return Array.from(this.#livros.entries())
 }
-
-    create(livro){
-        const livroId = randomUUID()
-        this.#livros.set(livroId, livro)
-    }
-    
-    update(id, livro){
-        this.#livros.set(id, livro)
-    }
-
-    delete(id, livro){
-        this.#livros.delete(id, livro)
-    }
+create(livro){
+    const livroId = randomUUID()
+    this.#livros.set(livroId, livro)
+}
+update(id, livro){
+    this.#livros.set(id, livro)
+}
+delete(id, livro){
+    this.#livros.delete(id, livro)
+}
 }
