ec795c0140505f926f977f85e5a3fa133c94118d
João Caetano <118355857+Caetano346@users.noreply.github.com>
2023-12-07 09:51:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,45 @@
-import { fastify } from 'fastify'
-import {DatabaseMemory} from "./database-memory.js"
+import {fastify} from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
 
-const server = fastify()
 const database = new DatabaseMemory()
+const server = fastify()
 
 server.get('/', () => {
-    return 'Olá Mundo'
+    return 'Rota padrão'
 })
 
 server.post('/livro', (request, reply) => {
-    //const body = request.body//
-   //console.log(body)//
-   const {titulo, autor, npaginas } = request.body
+    //acessando os dados do corpo da requisição
+    const {titulo, autor, npaginas} = request.body
+
+    //exibindo os dados
+    //console.log(body)
+
+   // return 'cadastrar'
     database.create({
-        titulo: 'livro 1',
-        autor: "autor 1 ",
-        npaginas: 400
+        titulo: titulo,
+        autor: autor,
+        npaginas: npaginas,
     })
-    console.log(database.list())
-    return reply.status(201).send()
+
+    return reply.status(201).send
 })
 
 server.get('/livro', () => {
-    return 'Ler!'
+    const livros = database.list()
+    console.log(livros)
+    return livros
+})
+
+server.put('/livros/:id', (request, reply) => {
+    const livroId = request.params.id
+    const {titulo, autor, npaginas} = request.body
+    const livro = database.update(livroId, {
+        titulo: titulo,
+        autor: autor,
+        npaginas: npaginas,  
+    })
+    return reply.status(204),send()
 })
 
 server.listen({
